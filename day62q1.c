19a1e1ef7160f920a318dc4a2b0b954928561dc8
Ayush77836 <singhsanjaykumar3866@gmail.com>
2026-04-09 22:49:21 +0530

    Add files via upload

---
+++ day62q1.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int index;
+    int edge;
+    int vertex;
+    struct Node* next;
+};
+
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+printf("enter the number of vertices: ");
+scanf("%d",&v);
+
+printf("enter number of indexes: ");
+sdcanf("%d",&i);
+
+printf("enter number of edges: ");
+scanf("%d",&e);
+
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Create graph
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
+
+    for (int i = 0; i < vertices; i++)
+        graph->adjLists[i] = NULL;
+
+    return graph;
+}
+
+// Add edge (undirected)
+void addEdge(struct Graph* graph, int src, int dest) {
+    // Add edge src -> dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    // Add edge dest -> src
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+// Print graph
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        struct Node* temp = graph->adjLists[i];
+        printf("%d: ", i);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    struct Graph* graph = createGraph(n);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);  
+    }
+
+    printf("\nAdjacency List:\n");
+    printGraph(graph);
+
+    return 0;
+}
