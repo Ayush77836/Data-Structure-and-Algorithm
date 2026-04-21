1c6ba22b1c28858040a48dd6ece013720d8de688
Ayush77836 <singhsanjaykumar3866@gmail.com>
2026-04-21 19:07:29 +0530

    Add files via upload

--- day80q1.c
+++ day80q1.c
@@ -1,71 +1,43 @@
 #include <stdio.h>
-#define INF 99999
+
+#define INF 1000000   // A large number to represent infinity
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
+    int n;
+    scanf("%d", &n);
 
-    int cost[100][100];
-    
-    // Initialize matrix
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(i == j)
-                cost[i][j] = 0;
+    int dist[n][n];
+
+    // Input and convert -1 to INF
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+            
+            if(dist[i][j] == -1 && i != j)
+                dist[i][j] = INF;
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Convert INF back to -1 and print result
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] == INF)
+                printf("-1 ");
             else
-                cost[i][j] = INF;
+                printf("%d ", dist[i][j]);
         }
-    }
-
-    // Input edges
-    int u, v, w;
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d %d", &u, &v, &w);
-        cost[u][v] = w;
-        cost[v][u] = w; // remove this line if graph is directed
-    }
-
-    int source;
-    scanf("%d", &source);
-
-    int dist[100], visited[100];
-
-    // Initialize distances
-    for(int i = 1; i <= n; i++) {
-        dist[i] = cost[source][i];
-        visited[i] = 0;
-    }
-
-    dist[source] = 0;
-    visited[source] = 1;
-
-    // Dijkstra Algorithm
-    for(int count = 1; count < n; count++) {
-        int min = INF, next = -1;
-
-        // Find minimum distance vertex
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i] && dist[i] < min) {
-                min = dist[i];
-                next = i;
-            }
-        }
-
-        if(next == -1) break;
-
-        visited[next] = 1;
-
-        // Update distances
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i] && dist[next] + cost[next][i] < dist[i]) {
-                dist[i] = dist[next] + cost[next][i];
-            }
-        }
-    }
-
-    // Output
-    for(int i = 1; i <= n; i++) {
-        printf("%d ", dist[i]);
+        printf("\n");
     }
 
     return 0;
